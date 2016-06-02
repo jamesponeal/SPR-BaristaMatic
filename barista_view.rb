7cfe1be4c8c4847f572e9830fb1714ba314fcc63
James O'Neal <jamesponeal@gmail.com>
2016-06-02 14:35:02 -0500

    adds inventory printing feature and inventory reducing method

--- barista_view.rb
+++ barista_view.rb
@@ -1,25 +1,38 @@
 class BaristaView
 
   def print_title
-    puts ""
-    wait_with_line(1,0.1)
+    wait_with_line(0.1)
     puts "~-"*15
-    wait_with_line(1,0.1)
+    wait_with_line(0.1)
     puts "           Welcome to"
-    wait_with_line(1,0.1)
+    wait_with_line(0.1)
     puts "          \e[32;1mBaristaMatic\e[0m"
-    wait_with_line(1,0.1)
+    wait_with_line(0.1)
     puts "     by SPR Coffee Services"
-    wait_with_line(1,0.1)
+    wait_with_line(0.1)
     puts "~-"*15
-    wait_with_line(1,0.1)
+    wait_with_line(0.1)
   end
 
-  def wait_with_line(number, seconds)
-    number.times do
-      sleep(seconds)
-      puts ""
+  def print_inventory(inventory)
+    wait_with_line(0.1)
+    puts "~-"*15
+    sleep(0.1)
+    puts "Inventory:"
+    sleep(0.1)
+    inventory.each do |item,qty|
+      puts " #{item},#{qty}"
+      sleep(0.1)
     end
+    sleep(0.1)
+    puts "~-"*15
+    wait_with_line(0.1)
+  end
+
+
+  def wait_with_line(seconds)
+    sleep(seconds)
+    puts ""
   end
 
 end
